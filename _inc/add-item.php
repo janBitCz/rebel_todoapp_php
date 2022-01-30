53732f8d4ca36cbd1da237928470554cd4eee251
janBitCz <bitterlichjan@gmail.com>
2022-01-30 22:04:11 +0100

    chyba

---
+++ _inc/add-item.php
@@ -0,0 +1,26 @@
+<?php
+
+	// include
+	require 'config.php';
+
+	// add new stuff
+	$id = $database->insert('items', [
+		'text' => $_POST['message']
+	]);
+
+	// success?
+	if ( ! $id ) die('error');
+
+	if ( is_ajax() ) {
+		header('Content-Type: application/json');
+
+		$message = json_encode([
+			'status' => 'success',
+			'id' => $id
+		]);
+
+		die( $message );
+	} else {
+		header("Location: $base_url/index.php");
+		die();
+	}
---
+++ _inc/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9a567ed5994f0b653fae3f3db42f0253d23ee5e0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9a567ed5994f0b653fae3f3db42f0253d23ee5e0',
+            'dev_requirement' => false,
+        ),
+        'catfan/medoo' => array(
+            'pretty_version' => 'v2.1.4',
+            'version' => '2.1.4.0',
+            'type' => 'framework',
+            'install_path' => __DIR__ . '/../catfan/medoo',
+            'aliases' => array(),
+            'reference' => '65042afa520475664863426d3b44d24604ab031c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
