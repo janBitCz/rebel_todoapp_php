dd3ef317d77d340ad9d1dd6dc82ca2853a3983a3
janBitCz <bitterlichjan@gmail.com>
2022-02-07 21:58:09 +0100

    delete whitespace

--- _inc/config.php
+++ _inc/config.php
@@ -5,10 +5,8 @@ ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 error_reporting(-1);
 
-
 // require stuff
 require_once 'vendor/autoload.php';
-
 
 // global variables
 require_once 'variables.php';
