e84c8d6309932c652e0d5f78d68050aea7fa8a6d
janBitCz <bitterlichjan@gmail.com>
2022-02-07 22:02:53 +0100

    edit whitespaces

--- _inc/config.php
+++ _inc/config.php
@@ -1,18 +1,18 @@
 <?php
 
-// show all errors
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(-1);
+	// show all errors
+	ini_set('display_startup_errors', 1);
+	ini_set('display_errors', 1);
+	error_reporting(-1);
 
-// require stuff
-require_once 'vendor/autoload.php';
+	// require stuff
+	require_once 'vendor/autoload.php';
 
-// global variables
-require_once 'variables.php';
+	// global variables
+	require_once 'variables.php';
 
-// connect to database
-require_once 'database.php';
+	// connect to database
+	require_once 'database.php';
 
-// global functions
-require_once 'functions.php';
+	// global functions
+	require_once 'functions.php';
